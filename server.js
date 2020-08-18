f826b3ea59f1ec95c827b8180f7d116c556b22f7
sandeep brahma <sandeep.brahma@gmail.com>
2020-08-18 09:34:24 +0800

    adding in git server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/bs4angmat'));
 
 app.listen(process.env.PORT || 4200);
 
 app.get('/*', (req, res) => {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-})
+    
+    res.sendFile('index.html',{root: 'dist/bs4angmat'});
+});
 
 
 console.log('Console listening!')
