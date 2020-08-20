b7044b35ffba6ec9927c99320f4b6fa94e29fe71
sandeep brahma <sandeep.brahma@gmail.com>
2020-08-20 13:13:38 +0800

    added annimations for contact and about me

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit, TemplateRef, Input, QueryList } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ViewChild, ElementRef } from '@angular/core';
+import { trigger, transition, style, animate, animateChild, query, stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    //transition('void => *', [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('.7s cubic-bezier(.8,-0.6,0.2,1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 
 export class ContactComponent implements OnInit {
--- src/app/pages/me/me.component.ts
+++ src/app/pages/me/me.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate, animateChild, query, stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
-  styleUrls: ['./me.component.scss']
+  styleUrls: ['./me.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
+    //transition('void => *', [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0)', opacity: 0 }),
+        animate('.7s cubic-bezier(.8,-0.6,0.2,1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class MeComponent implements OnInit {
 
